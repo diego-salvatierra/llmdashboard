a87382254759a699e4b44711775705e89b8709b6
Diego Salvatierra <diego@MacBook-Air-4.local>
2023-07-13 11:17:43 -0700

    added to github

--- .history/src/App_20230406141747.js
+++ .history/src/App_20230406141747.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './App.css';
 import AnalyticsDashboard from './AnalyticsDashboard';
 
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {/* Place the AnalyticsDashboard component here */}
         <AnalyticsDashboard />
       </header>
     </div>
--- .history/src/index_20230405184841.js
+++ .history/src/index_20230405184841.js
@@ -1,11 +1,8 @@
-import React, { useEffect, useState } from "react";
-import { createClient } from "@supabase/supabase-js";
-import { useTable } from "react-table";
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
